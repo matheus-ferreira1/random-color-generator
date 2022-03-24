5fa49b722c72fbefa746846a44a497ec46943f4c
Matheus Ferreira <matheus.fer33@gmail.com>
2022-03-23 22:23:26 -0300

    alteração no layout. mudança na lógica para que usuario escolha exibição em rgb ou hex. adicionado um toogle switch

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const myButton = document.getElementById('btn')
 myButton.addEventListener('click', changeColor)
-let myContainer = document.querySelector('.container')
+let checkbox = document.querySelector('.checkbox')
+checkbox.addEventListener('click', rgbOrHex)
+let cRed = ''
+let cGreen = ''
+let cBlue = ''
 
 function changeColor() {
+    let checkWrapper = document.querySelector('.main')
     let myBody = document.querySelector('body')
-    let cRed = Math.floor(Math.random()*255)
-    let cGreen = Math.floor(Math.random()*255)
-    let cBlue = Math.floor(Math.random()*255)
+    cRed = Math.floor(Math.random()*255)
+    cGreen = Math.floor(Math.random()*255)
+    cBlue = Math.floor(Math.random()*255)
     myBody.style.backgroundColor = `rgb(${cRed}, ${cGreen}, ${cBlue})`
+    
+    let textInjected = document.querySelector('#inject')
+    textInjected.innerHTML = `rgb(${cRed}, ${cGreen}, ${cBlue})`
+    textInjected.style.display = "block"
+    checkWrapper.style.display = "flex"
+}
 
-    let myText = document.createElement('h2')
-    myText.innerHTML = `COR EXIBIDA : rgb(${cRed}, ${cGreen}, ${cBlue})`
-    myText.className = 'new-btn'
-    myBody.appendChild(myText)
+function rgbOrHex() {
+    let textInjected = document.querySelector('#inject')
+
+    function colorToHex (color) {
+        let hexadecimal = color.toString(16);
+        return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+    }
+
+    function rgbToHex(red, green, blue) {
+        return '#' + colorToHex(red) + colorToHex(green) + colorToHex(blue);
+    }
+    
+
+    if (!checkbox.checked) {
+        textInjected.innerHTML = `rgb(${cRed}, ${cGreen}, ${cBlue})`
+    } else {
+        textInjected.innerHTML = rgbToHex(cRed, cGreen, cBlue)
+    }
 }
