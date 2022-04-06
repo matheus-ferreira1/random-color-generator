9c4d8218abe53839c6918feab75404eda652edb2
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-06 12:54:14 -0300

    corrigido bug que fazia com que ao mudar a cor com o seletor em hex, a nova cor exibida fosse em rgb

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ const myButton = document.getElementById('btn')
 myButton.addEventListener('click', changeColor)
 let checkbox = document.querySelector('.checkbox')
 checkbox.addEventListener('click', rgbOrHex)
+let textInjected = document.querySelector('#inject')
+
 let cRed = ''
 let cGreen = ''
 let cBlue = ''
+
+function colorToHex (color) {
+    let hexadecimal = color.toString(16);
+    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+}
+
+function rgbToHex(red, green, blue) {
+    return '#' + colorToHex(red) + colorToHex(green) + colorToHex(blue);
+}
 
 function changeColor() {
     let checkWrapper = document.querySelector('.main')
@@ -12,16 +23,21 @@ function changeColor() {
     cRed = Math.floor(Math.random()*255)
     cGreen = Math.floor(Math.random()*255)
     cBlue = Math.floor(Math.random()*255)
+
+    if (!checkbox.checked) {
+        textInjected.innerHTML = `rgb(${cRed}, ${cGreen}, ${cBlue})`
+    } 
+    if (checkbox.checked) {
+        textInjected.innerHTML = `${rgbToHex(cRed, cGreen, cBlue)}`
+    }
+
     myBody.style.backgroundColor = `rgb(${cRed}, ${cGreen}, ${cBlue})`
-    
-    let textInjected = document.querySelector('#inject')
-    textInjected.innerHTML = `rgb(${cRed}, ${cGreen}, ${cBlue})`
     textInjected.style.display = "block"
     checkWrapper.style.display = "flex"
 }
 
 function rgbOrHex() {
-    let textInjected = document.querySelector('#inject')
+   
 
     function colorToHex (color) {
         let hexadecimal = color.toString(16);
